47a0e6d998b85fd291168e0c38e13419550e0a4e
conors_nli <conor.sheehan.2@ucdconnect.ie>
2018-12-14 15:39:09 +0000

    fix tests for python 2

--- tests/test_split.py
+++ tests/test_split.py
@@ -3,12 +3,17 @@ import unittest
 from split import process_file
 from split import setup_params
 
+# handle python 2 v 3 cases
+try:
+  FileNotFoundError
+except:
+  FileNotFoundError = IOError
+
 class ProcessFile(unittest.TestCase):
   def test_process_file_not_found(self):
     with self.assertRaises(FileNotFoundError) as context:
       process_file('asdf', 'asdf')
 
-    # import pdb; pdb.set_trace()
     self.assertTrue("No such file or directory: 'asdf'" in str(context.exception)) 
 
 
