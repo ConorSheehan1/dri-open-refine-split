2bf82cae91fb60b9df828f3e8ffde65cb027c361
conors_nli <conor.sheehan.2@ucdconnect.ie>
2018-12-14 17:07:40 +0000

    moar tests

--- tests/test_split.py
+++ tests/test_split.py
@@ -8,6 +8,11 @@ import unittest
 from unittest.mock import patch
 
 class TestSplit(unittest.TestCase):
+  def __init__(self, methodName="runTest"):
+    unittest.TestCase.__init__(self, methodName)
+    self.filename = "./tests/fixtures/qdc1.xml"
+    self.outputdir = "./tests/fixtures/out"
+
   # https://stackoverflow.com/questions/4219717/how-to-assert-output-with-nosetest-unittest-in-python
   # output helper (hides pdb output too)
   def get_stdout(self):
@@ -22,16 +27,22 @@ class TestSplit(unittest.TestCase):
     self.assertTrue("No such file or directory: 'asdf'" in str(context.exception)) 
 
   def test_setup_params_cli_args(self):
-    filename = "./tests/fixtures/qdc1.xml"
-    outputdir = "./tests/fixtures/out"
-    args = argparse.Namespace(filename=filename, outputdir=outputdir)
-    # args = ["--filename " + filename, " --outputdir " + outputdir]
-    # with patch('argparse._sys.argv', args):
+    args = argparse.Namespace(filename=self.filename, outputdir=self.outputdir)
     with patch('argparse.ArgumentParser.parse_args',return_value=args):
-      self.assertEqual((filename, outputdir), setup_params())
+      self.assertEqual((self.filename, self.outputdir), setup_params())
       output = self.get_stdout()
-      self.assertTrue("Processing input file ./tests/fixtures/qdc1.xml" in output)
-      self.assertTrue("Creating output xml files in ./tests/fixtures/out" in output)
+      self.assertTrue("Processing input file " + self.filename in output)
+      self.assertTrue("Creating output xml files in " + self.outputdir in output)
+
+  def test_setup_params_invalid_params(self):
+    # if either the input file or output directory are invalid, print warning
+    for arg_list in [[self.filename, "asdf"], ["asdf", self.outputdir]]:
+      args = argparse.Namespace(filename=arg_list[0], outputdir=arg_list[1])
+      with patch('argparse.ArgumentParser.parse_args',return_value=args):
+        with self.assertRaises(SystemExit):
+          setup_params()
+          output = self.get_stdout()
+          self.assertTrue("Invalid input or output location" in output)
 
   def test_setup_params_gui_args(self):
     # import pdb; pdb.set_trace()
